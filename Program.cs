3eea2bfbbd798775bdc94d46a354cc6086f18dcf
spago <spago@DESKTOP-CSM61C1>
2024-01-17 14:44:46 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DrawingApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw pencil = new Draw();
+            Draw brush = new Brush();
+            Draw spray = new Spray();
+
+            pencil.StartDraw();
+            brush.StartDraw();
+            spray.StartDraw();
+        }
+    }
+}
